17c23b6de065a9413aa6c69bb9e4ead4886509ee
vineela <vineela19.kanaparthi@gmail.com>
2026-06-23 09:58:54 +0530

    ansible roles

--- roboshop.sh
+++ roboshop.sh
@@ -16,7 +16,7 @@ do
     RECORD_NAME="$instance.$DOMAIN_NAME"
   else
     IP=$(aws ec2 describe-instances --instance-ids $INSTANCE_ID --query "Reservations[0].Instances[0].PublicIpAddress" --output text)
-    RECORD_NAME="$DOMAIN_NAME"
+    RECORD_NAME="$instance.$DOMAIN_NAME"
   fi
   echo "$instance ip address: $IP"
 
